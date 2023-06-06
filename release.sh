0d095a7a9199885255a556de2c2ceab000a3fded
Louis Stekhoven-Smith <louis.stekhoven@gmail.com>
2023-06-07 00:59:21 +1000

    Improve release script

--- release.sh
+++ release.sh
@@ -7,6 +7,15 @@ if [ "$answer" != "y" ]; then
   exit 1
 fi
 
+local_commit=$(git rev-parse HEAD)
+remote_commit=$(git rev-parse origin/main)
+base_commit=$(git merge-base HEAD origin/main)
+
+if [ "$local_commit" != "$remote_commit" ] && [ "$local_commit" != "$base_commit" ]; then
+  echo "Your local branch has diverged from the remote branch. Please pull remote changes before deploying. Or remove the local changes you have"
+  exit 1
+fi
+
 if ! git diff-index --quiet HEAD --; then
   read -p "You have uncommitted changes. Would you like to stash them? (y/n): " answer
 
@@ -17,6 +26,13 @@ if ! git diff-index --quiet HEAD --; then
   git stash save --include-untracked
 fi
 
-git checkout main && git pull && git checkout -b release/$(date '+%Y-%m-%d')
+git checkout main && git pull
+branch_name="release/$(date '+%Y-%m-%d')"
+if git rev-parse --verify "$branch_name" >/dev/null 2>&1; then
+  git checkout "$branch_name" && git rebase main
+else
+  git checkout -b "$branch_name"
+fi
+
 git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)
 git checkout main
