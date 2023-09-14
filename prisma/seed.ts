32cfb8e90f7c2a8f15adeeeb15de505c6b92dcb0
Juan Ojeda <juanojeda@Juans-MacBook-Pro.local>
2023-09-14 11:38:41 +1000

    update seed to upsert instead of endlessly creating

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,13 +4,32 @@ export const prisma = new PrismaClient()
 
 async function runSeeds() {
   // Seed your data
-  const seedPostData = [{
-    authorId: 123,
-    title: "Creating a seed post"
-  }]
 
-  await prisma.post.createMany({
-    data: seedPostData
+  const SEED_USER_UUID = "my-unique-user-uuid"
+  const SEED_POST_UUID = "my-unique-post-uuid"
+
+  const seedUser = {
+    id: SEED_USER_UUID,
+    email: "seed.user@everest.engineering",
+    name: "Seedy McSeedface",
+  }
+
+  const seedPost = {
+    id: SEED_POST_UUID,
+    title: "Creatinga a seed post",
+    authorId: SEED_USER_UUID,
+  }
+
+  await prisma.user.upsert({
+    where: { id: seedUser.id },
+    create: seedUser,
+    update: seedUser,
+  })
+
+  await prisma.post.upsert({
+    where: { id: seedPost.id },
+    create: seedPost,
+    update: seedPost,
   })
 }
 
